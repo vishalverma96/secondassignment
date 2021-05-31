79dfbaa47b567ce426bc0a3edaf2341d0327913f
vishalverma96 <soni.vishu96@gmail.com>
2021-05-31 11:43:50 +0530

    firstpositive

--- Secondassignment/src/info/Infostride.java
+++ Secondassignment/src/info/Infostride.java
@@ -22,11 +22,35 @@ public class Infostride {
       
         return j;
     }
+	
+	static int firstOccPositve(int arr[], int low, int high)
+	{
+		if(high >= low)
+	    {
+	        int mid = (low + high)/2; /*low + (high - low)/2;*/
+	        if(mid == arr[mid]){
+	            return mid;
+	        }
+	        
+	        if(mid > arr[mid]){
+	             return firstOccPositve(arr, (mid + 1), high);
+	        }
+
+	        else{
+	            return firstOccPositve(arr, low, (mid -1));
+	        }
+	    }
+	    return -1;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("enter the length of array");
 		int size= s.nextInt();
+		if(size == 0) {
+			System.out.println("Not Found");
+			return;
+		}
 		
 		
 		int [] arr = new int[size+1];
@@ -36,10 +60,18 @@ public class Infostride {
 		
 		Arrays.sort(arr, 1,(arr.length));
 		int size2= removeDuplicates(arr, (arr.length));
-		System.out.println("Elements are:");
-		for (int i=1; i<size2; i++) {
-			System.out.println("Element in " +i+" Index: "+arr[i]);
-		}
+		int index = firstOccPositve(arr,1, (size2-1));
+		
+		if (index != -1){
+	        System.out.println("first positive :"+index);
+	       }
+	    else{
+	    	System.out.println("not found");
+	    }
+//		System.out.println("Elements are:");
+//		for (int i=1; i<size2; i++) {
+//			System.out.println("Element in " +i+" Index: "+arr[i]);
+//		}
 			
 
 	}
