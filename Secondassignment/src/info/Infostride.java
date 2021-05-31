ce397e1b37c564a0bc53eac1c7e6b6f419dce0ef
vishalverma96 <soni.vishu96@gmail.com>
2021-05-31 11:36:19 +0530

    remove_duplicates

--- Secondassignment/src/info/Infostride.java
+++ Secondassignment/src/info/Infostride.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 
 public class Infostride {
 	static Scanner s = new Scanner(System.in);
+	static int removeDuplicates(int arr[], int n)
+    {
+        if (n == 0 || n == 1)
+            return n;
+      
+        
+        int j = 1;
+      
+        
+        for (int i = 1; i < n-1; i++)
+            if (arr[i] != arr[i+1])
+                arr[j++] = arr[i];
+      
+        arr[j++] = arr[n-1];
+      
+        return j;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,9 +33,11 @@ public class Infostride {
 		for (int i=1; i<arr.length; i++) {
 			arr[i]=s.nextInt();
 		}
-		Arrays.sort(arr);
+		
+		Arrays.sort(arr, 1,(arr.length));
+		int size2= removeDuplicates(arr, (arr.length));
 		System.out.println("Elements are:");
-		for (int i=1; i<arr.length; i++) {
+		for (int i=1; i<size2; i++) {
 			System.out.println("Element in " +i+" Index: "+arr[i]);
 		}
 			
