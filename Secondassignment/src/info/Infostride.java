e6da577f892babb2c954167e96da76572040ca7a
vishalverma96 <soni.vishu96@gmail.com>
2021-05-31 11:18:30 +0530

    This is a main class

---
+++ Secondassignment/src/info/Infostride.java
@@ -0,0 +1,26 @@
+package info;
+
+import java.util.Scanner;
+
+public class Infostride {
+	static Scanner s = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the length of array");
+		int size= s.nextInt();
+		
+		
+		int [] arr = new int[size+1];
+		for (int i=1; i<arr.length; i++) {
+			arr[i]=s.nextInt();
+		}
+		System.out.println("Elements are:");
+		for (int i=1; i<arr.length; i++) {
+			System.out.println("Element in " +i+" Index: "+arr[i]);
+		}
+			
+
+	}
+
+}
